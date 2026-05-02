c1f57336c7f5fe5ef65c92c591275a9fdfe270bb
GavinJZhu <102933876+GavinJZhu@users.noreply.github.com>
2026-05-02 11:29:01 -0700

    finishing binary search

--- src/_02_Intro_To_Searching_Algorithms/_02_BinarySearch.java
+++ src/_02_Intro_To_Searching_Algorithms/_02_BinarySearch.java
@@ -17,22 +17,28 @@ public class _02_BinarySearch {
     public static int binarySearch(int[] array, int start, int end, int value) {
         // 2. if end is greater than or equal to start, then
         //    do steps 3 - 6.
+        if (end>=start) {
 
             // 3. create an integer called mid and set it equal
             //    to the half way point between start and end
-
+            int midIndex = array[start+((end-start)/2)];
             // 4. if the array element at mid is equal to value
             //    then return mid
-
+            if (midIndex == value){
+                return start+((end-start)/2);
+            }
             // 5. if the array element at mid is greater than value
             //    then return the value returned from a call to the 
             //    binarySearch method. Pass in start and mid - 1
             //    for the end variable.
-
+            else if (midIndex>value){
+                return binarySearch(array, start, midIndex-1, value);
+            }
             // 6. return the value returned from a call to the binarySearch
             //    method. Use mid + 1 as the start, and pass in end.
-
+            return binarySearch(array, midIndex+1, end, value);
+        }
         // 7. return -1 because the value was not found
-        return 0;
+        return -1;
     } 
 }
--- src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
+++ src/_02_Intro_To_Searching_Algorithms/_03_SearchTest.java
@@ -9,17 +9,24 @@ class _03_SearchTest {
     /*
      *  A minimum of 3 tests are required for each method
      */
-
+    int[] x = {1, 3, 4, 7, 8, 10, 12};
+    String[] words = {"day", "break", "front", "line", "orange", "star"};
     @Test
     public void testLinearSearch() {
         // 1. Use the assertEquals() method to test your linear search method
-        assertEquals(false, true);
+        assertEquals(3, _01_LinearSearch.linearSearch(words, "line"));
+        assertEquals(5, _01_LinearSearch.linearSearch(words, "star"));
+        assertEquals(1, _01_LinearSearch.linearSearch(words, "break"));
     }
 
     @Test
     public void testBinarySearch() {
         // 2. Use the assertEquals() method to test your binary search method
         //    remember that the array must be sorted
-        assertEquals(false, true);
+        assertEquals(3, _02_BinarySearch.binarySearch(x,0,x.length-1,7));
+        assertEquals(5, _02_BinarySearch.binarySearch(x,0,x.length-1,10));
+        assertEquals(0, _02_BinarySearch.binarySearch(x,0,x.length-1,1));
+
+
     }
 }
